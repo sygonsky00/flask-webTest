6b84aa02b8b4addd674c445d524cc290b2f13186
안신효 <sygonsky@naver.com>
2021-12-24 16:35:33 +0900

    webTest first commit

---
+++ myproject.cmd
@@ -0,0 +1,8 @@
+@echo off
+cd/
+E:
+cd \project\SampleProject\playGround\python\venv\Scripts
+set FLASK_APP=webTest
+set FLASK_ENV=development
+activate
+
---
+++ webTest/views/main_views.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+
+@bp.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+
+@bp.route('/')
+def index():
+    return redirect(url_for('question._list'))
+

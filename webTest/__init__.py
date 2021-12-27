b4f48de3e3df2348dd32db72ae88ad062f76617a
안신효 <sygonsky@naver.com>
2021-12-27 10:57:16 +0900

    404 오류등록

--- webTest/__init__.py
+++ webTest/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -14,6 +14,10 @@ naming_convention = {
 }
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
+
+
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 def create_app():
@@ -43,5 +47,8 @@ def create_app():
 
     # markdown
     Markdown(app, extensions=['nl2br', 'fenced_code'])
+
+    # 오류페이지
+    app.register_error_handler(404, page_not_found)
 
     return app
